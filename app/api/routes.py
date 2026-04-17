39301a6428802f654e2b7c915b48ceae7a4c58b1
Alexander <schwarzepolizei@gmail.com>
2026-04-17 13:02:20 +0400

    Добавлен ответ модели для эндпоинта обработки файла

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, UploadFile, File
 from app.pipeline.etl_pipeline import run_etl
+from app.schemas.document import ETLResponse
 
 router = APIRouter()
 
 
-@router.post("/process")
+@router.post("/process", response_model=ETLResponse)
 async def process_file(file: UploadFile = File(...)):
     result = run_etl(file.filename)
     return result
