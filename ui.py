833d3c7d5e1d2292a856f404a31c351b13841546
Alexander <schwarzepolizei@gmail.com>
2026-05-04 16:47:21 +0400

    Изменен URL API на "http://api:8000" для корректной работы с контейнерами Docker.

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st
 
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "http://api:8000"
 
 
 st.set_page_config(page_title="ETL + RAG", page_icon="📄", layout="wide")
